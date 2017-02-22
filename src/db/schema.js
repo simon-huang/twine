d92c4fe1bae93cc883caa716c9ceda1d06790b93
Abiy Melaku <agirma08@gmail.com>
2017-02-21 18:10:47 -0800

    Design sql schema

--- src/db/schema.js
+++ src/db/schema.js
@@ -8,38 +8,19 @@ var sequelize = new Sequelize('publishus', 'root', '');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/publishus');
 
-
-/*
 // Create User model
 var User = sequelize.define('user', {
-  username: {type: Sequelize.STRING, unique: true},
-  password: Sequelize.STRING
+  // username: {type: Sequelize.STRING, unique: true},
+  email: {type: Sequelize.STRING, unique: true},
+  password: Sequelize.STRING,
 });
+
+
 // Sync all models and associations
 sequelize.sync();
 
 module.exports.User = User;
 
 
-// For adding new users (ADD TO NEW FILE)
-// If added to new file, make sure to require schema.js
-
-// db.User.create({
-//   username: 'theUsername',
-//   password: 'theirPassword'
-// })
-*/
 
 
-/*
-// methods ======================
-// generating a hash
-User.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-User.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-*/
