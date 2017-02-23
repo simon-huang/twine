d28e30dade957ed609aca696ab254436d01ed300
Abiy Melaku <agirma08@gmail.com>
2017-02-23 10:40:51 -0800

    Fix typo

--- src/app/reducers/index.jsx
+++ src/app/reducers/index.jsx
@@ -1,7 +1,7 @@
 // This is the reducer that aggregates all other reducers
 import { combineReducers } from "redux";
 
-import doc from "./documentReducer.jsx";
+import doc from "./docReducer.jsx";
 import docSummary from "./docSummaryReducer.jsx";
 import nav from "./navReducer.jsx";
 import user from "./userReducer.jsx";
