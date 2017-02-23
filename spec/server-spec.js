7cc0a4d6bc6ceda6b2eee612a7228c8abd0eca59
Franklin Jeng <franklinjjeng@gmail.com>
2017-02-23 13:41:33 -0800

    spec set up

--- spec/server-spec.js
+++ spec/server-spec.js
@@ -1,1 +1,32 @@
-var expect = require('chai').expect;
+// var expect = require('chai').expect;
+
+import React from 'react';
+import sinon from 'sinon';
+import { mount, shallow } from 'enzyme';
+
+import Foo from './Foo';
+
+describe('<Foo />', () => {
+  it('allows us to set props', () => {
+    const wrapper = mount(<Foo bar="baz" />);
+    expect(wrapper.props().bar).to.equal('baz');
+    wrapper.setProps({ bar: 'foo' });
+    expect(wrapper.props().bar).to.equal('foo');
+  });
+
+  it('simulates click events', () => {
+    const onButtonClick = sinon.spy();
+    const wrapper = mount(
+      <Foo onButtonClick={onButtonClick} />
+    );
+    wrapper.find('button').simulate('click');
+    expect(onButtonClick).to.have.property('callCount', 1);
+  });
+
+  it('calls componentDidMount', () => {
+    sinon.spy(Foo.prototype, 'componentDidMount');
+    const wrapper = mount(<Foo />);
+    expect(Foo.prototype.componentDidMount).to.have.property('callCount', 1);
+    Foo.prototype.componentDidMount.restore();
+  });
+});
