e734fd0149bc680576c46322808ace4ed982e5c5
Justin Mendelson <jmendelson22@gmail.com>
2017-02-25 17:14:25 -0800

    Deleting comments

--- src/app/components/document/doc_contents.jsx
+++ src/app/components/document/doc_contents.jsx
@@ -17,23 +17,3 @@ class Doc_contents extends React.Component {
 
 
 export default connect(state => state)(Doc_contents);
-
-
-
-
-//dummy data for content
-        // <h1>Graphene Oxide Dispersions in Organic Solvents</h1>
-        // <br/>
-        // <p>J. I. Paredes*, S. Villar-Rodil, A. Martínez-Alonso and J. M. D. Tascón</p>
-        // <p>Instituto Nacional del Carbón, CSIC, Apartado 73, 33080 Oviedo, Spain</p>
-        // <br/>
-        // <p>Langmuir, 2008, 24 (19), pp 10560–10564</p>
-        // <p>DOI: 10.1021/la801744a</p>
-        // <p>Publication Date (Web): August 29, 2008</p>
-        // <p>Copyright © 2008 American Chemical Society</p>
-        // <br/>
-        // <h3>Abstract</h3>
-        // <br/>
-        // <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRyPsOaa9Zj95oq1Pc6gt6_mw2juf4Zy7t_BG-DUD7StagL4EYpmg"/>
-        // <br/>
-        // <p>The dispersion behavior of graphene oxide in different organic solvents has been investigated. As-prepared graphite oxide could be dispersed in N,N-dimethylformamide, N-methyl-2-pyrrolidone, tetrahydrofuran, and ethylene glycol. In all of these solvents, full exfoliation of the graphite oxide material into individual, single-layer graphene oxide sheets was achieved by sonication. The graphene oxide dispersions exhibited long-term stability and were made of sheets between a few hundred nanometers and a few micrometers large, similar to the case of graphene oxide dispersions in water. These results should facilitate the manipulation and processing of graphene-based materials for different applications.</p>
