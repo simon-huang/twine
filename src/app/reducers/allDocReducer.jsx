8b39c9fecf258e8ebe5284913688ed6a620d0d4f
Justin Mendelson <jmendelson22@gmail.com>
2017-02-25 17:15:15 -0800

    delete comments

--- src/app/reducers/allDocReducer.jsx
+++ src/app/reducers/allDocReducer.jsx
@@ -1,11 +1,5 @@
 export default function reducer(state = {
-  allDocuments: [
-  //======dummy data==========//
-    // {docName: 'Doc1', docDescription: 'Doc1 description', type: ''},
-    // {docName: 'Doc2', docDescription: 'Doc2 description', type: ''},
-    // {docName: 'Doc3', docDescription: 'Doc3 description', type: ''},
-    // {docName: 'Doc4', docDescription: 'Doc4 description', type: ''}
-  ],
+  allDocuments: [],
   error: null
 }, action) {
 
