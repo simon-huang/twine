ea34895268878e7fe43e8452440ace2aa7f30069
Franklin Jeng <franklinjjeng@gmail.com>
2017-02-26 19:43:27 -0800

    (refactor) converting connecting/store initialization. Redirect now goes to profile

--- src/app/reducers/userReducer.jsx
+++ src/app/reducers/userReducer.jsx
@@ -5,7 +5,7 @@ export default function reducer(state = {
     email: '',
     password: ''
   },
-  redirectUrl: '/doc',
+  redirectUrl: '/profile',
   fetching: false,
   fetched: false,
   error: null
