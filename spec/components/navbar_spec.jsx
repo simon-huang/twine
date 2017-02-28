1bff20dfd1dd73f99d518c2540d7cee166efc149
Justin Mendelson <jmendelson22@gmail.com>
2017-02-28 11:38:27 -0800

    (test) navbar tests

--- spec/components/navbar_spec.jsx
+++ spec/components/navbar_spec.jsx
@@ -10,25 +10,36 @@ import * as user from '../../src/app/actions/userActions.jsx';
 describe('Navbar', () => {
 
   const dummydata = {
-    username: 'iamgroot',
     location: {
       pathname: '/'
     }
   }
 
-  const wrapper = mount(
-    <Navbar props={dummydata} store={store} />
+  const shallowWrapper = shallow (
+    <Navbar dispatch={store.dispatch} props={dummydata} />
   );
 
-  console.log('components methods', wrapper.instance());
-  console.log('login button', wrapper.find('NavItem').nodes[0].props);
-  console.log('store', wrapper.props().store.getState().user.loginModal);
+  const mountedWrapper = mount (
+    <Navbar dispatch={store.dispatch} props={dummydata} store={store} />
+  );
+
+  // console.log('store', shallowWrapper);
+  // console.log('.login', shallowWrapper.find('[name="login"]'));
+  // console.log('NavItem.login', shallowWrapper.find('NavItem .login'));
+  // console.log('NavItem[0]', shallowWrapper.find('NavItem'));
+  // console.log('mounted wrapper state', mountedWrapper.props().store.getState());
 
   it('should toggle login modal', () => {
-    const loginBtn = wrapper.find('NavItem').nodes[0].props;
-    const loginModal = wrapper.props().store.getState().user.loginModal;
-    expect(loginBtn.name).to.equal('login');
-    loginBtn.onClick();
+    // const loginBtn = shallowWrapper.find('[name="login"]').node.props;
+    // const loginModal = wrapper.props().store.getState().user.loginModal;
+    // loginBtn.onClick();
+    // console.log('after click (state)', mountedWrapper.props().store.getState());
   });
 
 });
+
+
+
+
+
+
