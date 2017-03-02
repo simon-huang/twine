38dca546642c60cdb0c4824f75d81f80515136b1
Justin Mendelson <jmendelson22@gmail.com>
2017-03-02 11:56:36 -0800

    (feat) persist document into merge interface

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -4,13 +4,15 @@ import { connect } from 'react-redux';
 class Home extends React.Component {
   render() {
     return (
-      <div className="fold_one">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <h1 className="mb25">Collaboratively publish papers online</h1>
-              <h4 className="mb40">Document creation that lets you manage versions contributors easily</h4>
-              <img src="/assets/img/doc_preview.png"/>
+      <div className="home-page">
+        <div className="fold_one">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 text-center">
+                <h1 className="mb25">Collaboratively publish papers online</h1>
+                <h4 className="mb40">Document creation that lets you manage versions contributors easily</h4>
+                <img src="/assets/img/doc_preview.png"/>
+              </div>
             </div>
           </div>
         </div>
