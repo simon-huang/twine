63259341ffd3b8dc3ed2d102f510445b22fb3cf9
Abiy Melaku <agirma08@gmail.com>
2017-03-02 21:30:30 -0800

    Write componentWillMount lifecycle event

--- src/app/components/document/doc_details.jsx
+++ src/app/components/document/doc_details.jsx
@@ -9,15 +9,19 @@ import Button from 'react-bootstrap/lib/Button';
 import DocContents from './doc_contents.jsx'
 
 //actions
-import { retrieveSpecificDoc } from '../../actions/allDocActions.jsx'
+import { retrieveSpecificDoc } from '../../actions/docActions.jsx'
 
 export class Doc_details extends React.Component {
+  
   constructor(props) {
     super(props);
   }
-  componentDidMount() {
-    const username = this.props.user.username;
-    const docID = this.props.doc.docId || 5;
+  
+  componentWillMount() {
+    var path = window.location.pathname;
+    var splitPath = path.split(/[\\\/]/);
+    const username = splitPath[splitPath.length - 2];
+    const docID = splitPath[splitPath.length - 1];
     this.props.dispatch(retrieveSpecificDoc(username, docID));
   }
 
@@ -28,10 +32,7 @@ export class Doc_details extends React.Component {
           <div className="col-sm-12">
             <div className="row mt10">
               <div className="col-sm-12">
-                <div className="doc-desc">
-                  {console.log('this.props.username', this.props.user.username)}                
-                  {console.log('this.props.docID', this.props.doc.docId)}  
-                  {console.log('this.props.doc.docDescription', this.props.doc.docDescription)}              
+                <div className="doc-desc">             
                   {this.props.doc.docDescription}
                 </div>
               </div>  
