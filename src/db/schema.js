acd1a9e8d2c6fa5937b05ca3a7afd6e59687dce5
Simon Huang <simon.t.huang@gmail.com>
2021-03-03 20:16:33 -0800

    Specify dialect

--- src/db/schema.js
+++ src/db/schema.js
@@ -2,12 +2,15 @@
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('publishus', 'root', '');
+var sequelize = new Sequelize('publishus', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 // Create User model
 var User = sequelize.define('user', {
-  username: {type: Sequelize.STRING, unique: true},
-  email: {type: Sequelize.STRING, unique: true},
+  username: { type: Sequelize.STRING, unique: true },
+  email: { type: Sequelize.STRING, unique: true },
   password: Sequelize.STRING
 });
 
@@ -18,8 +21,8 @@ var Doc = sequelize.define('doc', {
   name: Sequelize.STRING,
   description: Sequelize.STRING,
   filepath: Sequelize.STRING,
-  public: Sequelize.BOOLEAN, 
-  timeCreated: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
+  public: Sequelize.BOOLEAN,
+  timeCreated: { type: Sequelize.DATE, defaultValue: Sequelize.NOW },
   userId: {
     type: Sequelize.STRING,
     primaryKey: false,
@@ -48,11 +51,11 @@ var Doc = sequelize.define('doc', {
 
 // Create DocVersion model
 // Are we letting the user see all saves (commits) in their version history?
-  // if not, add another column to differentiate between "quick saves" and saves with a comment
+// if not, add another column to differentiate between "quick saves" and saves with a comment
 var DocVersion = sequelize.define('docVersion', {
-  commitID: {type: Sequelize.STRING, unique: true},
+  commitID: { type: Sequelize.STRING, unique: true },
   commitMessage: Sequelize.STRING,
-  timeCreated: {type: Sequelize.DATE, defaultValue: Sequelize.NOW} 
+  timeCreated: { type: Sequelize.DATE, defaultValue: Sequelize.NOW }
 });
 DocVersion.belongsTo(Doc);
 DocVersion.belongsTo(User);
@@ -60,14 +63,14 @@ DocVersion.belongsTo(User);
 // Create DocPermission model
 // type: owner or collaborator
 var DocPermission = sequelize.define('docPermission', {
-  type: Sequelize.STRING 
+  type: Sequelize.STRING
 });
 DocPermission.belongsTo(Doc);
 DocPermission.belongsTo(User);
 
 // (accept/decline = closed, open = pending)
 var PullRequest = sequelize.define('pullRequest', {
-  status: Sequelize.STRING, 
+  status: Sequelize.STRING,
   collaboratorMessage: Sequelize.STRING,
   ownerMessage: Sequelize.STRING,
   requesterName: {
